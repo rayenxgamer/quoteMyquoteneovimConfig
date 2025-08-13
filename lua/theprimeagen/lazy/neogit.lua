708154b08f8895bd8b2c9445ab34422ef9eaef78
rayenxgamer <rayenxgamer@gmail.com>
2025-08-13 20:09:19 +0100

    made neogit work better.!

--- lua/theprimeagen/lazy/neogit.lua
+++ lua/theprimeagen/lazy/neogit.lua
@@ -1,7 +1,18 @@
 return {
   'NeogitOrg/neogit',
-   config = function()
-    local comment = require("neogit")
-    comment:setup()
-   end,
+    dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+
+    -- Only one of these is needed.
+    "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua",              -- optional
+    "echasnovski/mini.pick",         -- optional
+    "folke/snacks.nvim",             -- optional
+  },
+
+   -- config = function()
+   --  local comment = require("neogit")
+   --  comment:setup()
+   -- end,
 }
